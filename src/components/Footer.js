7a334447138822d273f26eff5e1ee92c18633c8c
Your Name <prashantyadav132003@gmail.com>
2025-08-25 00:36:45 +0530

    yadav2

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Designed and Developed by Prashant Yadav</h3>
+          <h3>Designed and Developed by Codewingx</h3>
         </Col>
         <Col md="4" className="footer-copywright">
           <h3>Copyright © {year} PY</h3>
